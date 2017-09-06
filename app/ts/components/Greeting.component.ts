448b011351f9c92badc42712d7be530abbb1968c
bypixelpro <info@pixelpro.es>
2017-09-06 11:12:46 +0200

    Quitamos elementos para producción

--- app/ts/components/Greeting.component.ts
+++
@@ -1,8 +0,0 @@
-export class GreetingComponent {
-
-    constructor(public _name: string) {}
-
-    say(): string {
-        return `Hello from TypeScript ${this._name}, welcome!.`;
-    }
-}
--- app/ts/main.ts
+++
@@ -1,4 +0,0 @@
-import {GreetingComponent} from './components/Greeting.component';
-
-let say = new GreetingComponent('Dani');
-alert(say.say());
